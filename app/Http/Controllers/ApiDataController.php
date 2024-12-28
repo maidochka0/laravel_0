04ec5bee073276ec07a3c22236facaf60ac097aa
lol <lol.lol>
2024-12-28 07:07:35 +0100

    fetch-sales

--- app/Http/Controllers/ApiDataController.php
+++ app/Http/Controllers/ApiDataController.php
@@ -35,55 +35,61 @@ class ApiDataController extends Controller
         if (!$limit) $limit = 500;
         if (!$key) $key = All::getPrivateKey();
 
-        $response = Http::get("http://89.108.115.241:6969/api/sales", [
-            'dateFrom' => $dateFrom,
-            'dateTo' => $dateTo,
-            'page' => $page,
-            'key' => $key,
-            'limit' => $limit,
-        ]);
+        $isLastPage = ($limit === 500) ? false : true;
+        while ($isLastPage === false) {
+            $response = Http::get("http://89.108.115.241:6969/api/sales", [
+                'dateFrom' => $dateFrom,
+                'dateTo' => $dateTo,
+                'page' => $page,
+                'key' => $key,
+                'limit' => $limit,
+            ]);
 
-        if ($response->successful()) {
-            $data = $response->json()['data'];
-            $accountId = $this->getAccountIdByToken($key);
+            if ($response->successful()) {
+                $data = $response->json()['data'];
+                $accountId = $this->getAccountIdByToken($key);
 
-            foreach ($data as $saleData) {
-                Sale::create([
-                    'g_number' => $saleData['g_number'],
-                    'date' => $saleData['date'],
-                    'last_change_date' => $saleData['last_change_date'],
-                    'supplier_article' => $saleData['supplier_article'],
-                    'tech_size' => $saleData['tech_size'],
-                    'barcode' => $saleData['barcode'],
-                    'total_price' => $saleData['total_price'],
-                    'discount_percent' => $saleData['discount_percent'],
-                    'is_supply' => $saleData['is_supply'],
-                    'is_realization' => $saleData['is_realization'],
-                    'promo_code_discount' => $saleData['promo_code_discount'],
-                    'warehouse_name' => $saleData['warehouse_name'],
-                    'country_name' => $saleData['country_name'],
-                    'oblast_okrug_name' => $saleData['oblast_okrug_name'],
-                    'region_name' => $saleData['region_name'],
-                    'income_id' => $saleData['income_id'],
-                    'sale_id' => $saleData['sale_id'],
-                    'odid' => $saleData['odid'],
-                    'spp' => $saleData['spp'],
-                    'for_pay' => $saleData['for_pay'],
-                    'finished_price' => $saleData['finished_price'],
-                    'price_with_disc' => $saleData['price_with_disc'],
-                    'nm_id' => $saleData['nm_id'],
-                    'subject' => $saleData['subject'],
-                    'category' => $saleData['category'],
-                    'brand' => $saleData['brand'],
-                    'is_storno' => $saleData['is_storno'],
-                    'account_id' => $accountId
-                ]);
-            }
+                $salesData = [];
+                foreach ($data as $saleData) {
+                    $salesData[] = [
+                        'g_number' => $saleData['g_number'],
+                        'date' => $saleData['date'],
+                        'last_change_date' => $saleData['last_change_date'],
+                        'supplier_article' => $saleData['supplier_article'],
+                        'tech_size' => $saleData['tech_size'],
+                        'barcode' => $saleData['barcode'],
+                        'total_price' => $saleData['total_price'],
+                        'discount_percent' => $saleData['discount_percent'],
+                        'is_supply' => $saleData['is_supply'],
+                        'is_realization' => $saleData['is_realization'],
+                        'promo_code_discount' => $saleData['promo_code_discount'],
+                        'warehouse_name' => $saleData['warehouse_name'],
+                        'country_name' => $saleData['country_name'],
+                        'oblast_okrug_name' => $saleData['oblast_okrug_name'],
+                        'region_name' => $saleData['region_name'],
+                        'income_id' => $saleData['income_id'],
+                        'sale_id' => $saleData['sale_id'],
+                        'odid' => $saleData['odid'],
+                        'spp' => $saleData['spp'],
+                        'for_pay' => $saleData['for_pay'],
+                        'finished_price' => $saleData['finished_price'],
+                        'price_with_disc' => $saleData['price_with_disc'],
+                        'nm_id' => $saleData['nm_id'],
+                        'subject' => $saleData['subject'],
+                        'category' => $saleData['category'],
+                        'brand' => $saleData['brand'],
+                        'is_storno' => $saleData['is_storno'],
+                        'account_id' => $accountId
+                    ];
+                }
+                Sale::insert($salesData);
+            } else return response()->json(['message' => 'unsucc'], 500);
 
-            return response()->json(['message' => 'succ']);
+            if (++$page === $response->json()['meta']['last_page']) $isLastPage = true;
         }
+        return response()->json(['message' => 'succ']);
 
-        return response()->json(['message' => 'unsucc'], 500);
+        
     }
 
     public function fetchIncomes(Request $request)
