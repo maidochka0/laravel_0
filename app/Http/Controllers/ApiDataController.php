9108f56a0e39733a97dbe1036724f57585726089
lol <lol.lol>
2024-12-30 23:44:00 +0100

    realise

--- app/Http/Controllers/ApiDataController.php
+++ app/Http/Controllers/ApiDataController.php
@@ -26,83 +26,10 @@ class ApiDataController extends Controller
     {
         if ($errResponse = $this->handleRateLimiting($request)) return $errResponse;
 
-        $dateFrom = $request->input('dateFrom');
-        $dateTo = $request->input('dateTo');
-        $limit = $request->input('limit');
-        $page = $request->input('page');
         $key = $request->input('key');
-
-        if (!$page) $page = 1;
-        if (!$limit) $limit = 500;
-        if (!$dateFrom) $dateFrom = '2000-01-01';
-        if (!$dateTo) $dateTo = date('Y-m-d');
         if (!$key) $key = All::getPrivateKey();
 
-        $isLastPage = false;
-        while ($isLastPage === false) {
-            $response = Http::get("http://89.108.115.241:6969/api/sales", [
-                'dateFrom' => $dateFrom,
-                'dateTo' => $dateTo,
-                'page' => $page,
-                'key' => $key,
-                'limit' => $limit,
-            ]);
-
-            if ($response->successful()) {
-                $data = $response->json()['data'];
-
-                $accountId = null;//$this->getAccountIdByToken($key);
-                $this->addAccountIdInData($data, $accountId);
-                Sale::insert($data);
-                gc_collect_cycles();//!!!!без этой хуйни все падает с превышением выделенной памяти. 
-                /*foreach ($data as $saleData) {
-                    $sales = [
-                        'g_number' => $saleData['g_number'],
-                        'date' => $saleData['date'],
-                        'last_change_date' => $saleData['last_change_date'],
-                        'supplier_article' => $saleData['supplier_article'],
-                        'tech_size' => $saleData['tech_size'],
-                        'barcode' => $saleData['barcode'],
-                        'total_price' => $saleData['total_price'],
-                        'discount_percent' => $saleData['discount_percent'],
-                        'is_supply' => $saleData['is_supply'],
-                        'is_realization' => $saleData['is_realization'],
-                        'promo_code_discount' => $saleData['promo_code_discount'],
-                        'warehouse_name' => $saleData['warehouse_name'],
-                        'country_name' => $saleData['country_name'],
-                        'oblast_okrug_name' => $saleData['oblast_okrug_name'],
-                        'region_name' => $saleData['region_name'],
-                        'income_id' => $saleData['income_id'],
-                        'sale_id' => $saleData['sale_id'],
-                        'odid' => $saleData['odid'],
-                        'spp' => $saleData['spp'],
-                        'for_pay' => $saleData['for_pay'],
-                        'finished_price' => $saleData['finished_price'],
-                        'price_with_disc' => $saleData['price_with_disc'],
-                        'nm_id' => $saleData['nm_id'],
-                        'subject' => $saleData['subject'],
-                        'category' => $saleData['category'],
-                        'brand' => $saleData['brand'],
-                        'is_storno' => $saleData['is_storno'],
-                        'account_id' => $accountId
-                    ];
-                    Sale::insert($sales);
-                }*/
-                
-            } else if ($response->status() === 429) {
-                $retryAfter = $response->header('Retry-After', 1); // Получаем время ожидания, если указано, или 1 секунда по умолчанию
-                echo "429_$retryAfter\n";
-                sleep($retryAfter); // Ждем указанное время перед повторной попыткой
-                continue; // Переходим к следующей итерации циклаreturn response()->json(['message' => 'unsucc'], 500);
-            } else return response()->json(['message' => 'unsucc response'], 500);
-
-            print "$page|"; if ($page % 10 === 0) print "<br>\n";
-            if ($page++ === $response->json()['meta']['last_page']) $isLastPage = true;
-        }
-        print "<hr>!";print $response->json()['meta']['last_page'];
-        return response()->json(['message' => 'succ']);
-
-        
+        return $this->add('sales', $key, $request);
     }
 
     public function fetchIncomes(Request $request)
@@ -121,6 +48,9 @@ class ApiDataController extends Controller
 
         $key = $request->input('key');
         if (!$key) $key = All::getPrivateKey();
+        
+        $date = date('Y-m-d');
+        $request->merge(['dateFrom'=>$date, 'dateTo'=>$date]);
 
         return $this->add('stocks', $key, $request);
     }
@@ -135,24 +65,27 @@ class ApiDataController extends Controller
         return $this->add('orders', $key, $request);
     }
 
-    private function add($table, $token, Request $request)
+    private function add($table, $key, Request $request)
     {
-        $accountId = $this->getAccountIdByToken($token);
-        DB::table($table)->where('account_id', $accountId)->delete(); //fresh
+        $accountId = $this->getAccountIdByToken($key);
 
         $dateFrom = $request->input('dateFrom');
         $dateTo = $request->input('dateTo');
         $page = $request->input('page');
         $limit = $request->input('limit');
-        $key = $request->input('key');
 
         if (!$page) $page = 1;
         if (!$limit) $limit = 500;
         if (!$dateFrom) $dateFrom =  ($table === 'stocks') ? date('Y-m-d') : '2000-01-01';
         if (!$dateTo) $dateTo = date('Y-m-d');
-        if (!$key) $key = All::getPrivateKey();
+
+        DB::table($table)
+            ->where('account_id', $accountId)
+            ->whereBetween('date', [$dateFrom, $dateTo])
+            ->delete();
 
         $isLastPage = false;
+        print "table $table:\n";
         while (!$isLastPage) {
             $response = Http::get("http://89.108.115.241:6969/api/$table", [
                 'dateFrom' => $dateFrom,
@@ -190,37 +123,19 @@ class ApiDataController extends Controller
         }, $data);
     }
 
-    public static function dailyUpdate()
+    public function dailyUpdate(request $request)
     {
         //эта функция не валидируется на количество запросов, т.к. запускается только из заданого крон. и вообще она в целом держиться как обособленная функция чисто под 1 задачу для крон, как-будто ей не место в контролере. пришлось прибегнуть к такому костыльному решению, т.к. не смог нормально настроить ежедневный вызов через встроенные инструменты ларавель.
         
-        $dateFrom = date('Y-m-d');
-        $dateTo = date('Y-m-d');
-        $page = 1;
-        $limit = 500;
-        
+        $key = $request->input('key');
+        if (!$key) $key = All::getPrivateKey();
+
         foreach(ALL::getTables() as $table) {
-            $response = Http::get("http://89.108.115.241:6969/api/$table", [
-                'dateFrom' => $dateFrom,
-                'dateTo' => $dateTo,
-                'page' => $page,
-                'key' => All::getPrivateKey(),
-                'limit' => $limit,
-            ]);
-
-            if (!$response->successful()) return "$table-err";
-
-            $data = $response->json()['data'];
-            $data = array_map(function($item) {
-                $item['account_id'] = CRON_ACCOUNT_ID;
-                return $item;
-            }, $data);
-
-            DB::table($table)->insertOrIgnore($data); //можно добавить проверку на дубликаты из бд. хотя все было бы проще, если бы апи возвращал индексированные записи
+            print $this->add($table, $key, $request);
         }
 
         //print in cron.log
-        print "daily update succ\n";
+        print "\ndaily update succ\n";
         print "-\n";
     }
 
--- cron_src.php
+++ cron_src.php
@@ -1,6 +1,10 @@
 <?php
 
-$url = 'http://193.233.114.253:9001/daily-update';
+define('CRON_TOKEN', '');
+
+$date = date('Y-m-d');
+$token = CRON_TOKEN;
+$url = "http://193.233.114.253:9001/daily-update/?dateFrom=$date&key=$token";
 
 $response = file_get_contents($url);
 
